1ff092c24b9ce8e61cc75dbddbb53eb6284be004
Binam Kayastha <binamkayastha@example.com>
2017-12-10 02:29:46 -0500

    Update new method of generating label data
    Adjusted for mapping and no more cropping.

--- navirice_generate_data.py
+++ navirice_generate_data.py
@@ -1,12 +1,14 @@
 from navirice_get_image import navirice_get_image
 from navirice_head_detect import get_head_from_img
 from navirice_helpers import navirice_image_to_np
+from navirice_helpers import map_depth_and_rgb
 
 import cv2
 import numpy as np
 
 DEFAULT_HOST = '127.0.0.1'  # The remote host
 DEFAULT_PORT = 29000        # The same port as used by the server
+
 
 def main():
     """Main to test this function. Should not be run for any other reason."""
@@ -19,30 +21,59 @@ def main():
         if img_set is None:
             continue
 
-        depth_image = navirice_image_to_np(img_set.Depth)
         rgb_image = navirice_image_to_np(img_set.RGB)
-        possible_head_data = get_head_from_img(rgb_image)
+        depth_image = navirice_image_to_np(img_set.Depth)
+        (rgb_image, depth_image) = map_depth_and_rgb(rgb_image, depth_image)
 
-        working_image = depth_image
+        possible_bitmap = generate_bitmap_label(rgb_image, depth_image)
 
-        if possible_head_data is None:
+        if possible_bitmap is None:
             print("No head detected")
-            #cv2.imshow("working", working_image)
+            cv2.imshow("depth", depth_image)
             continue
 
-        (x, y, radius) = possible_head_data
-
-        (scaled_x, scaled_y, scaled_radius) = get_scaled(x, y, radius, working_image)
-
-        resize_scale = 0.3
-        #cv2.circle(working_image, (scaled_x, scaled_y), scaled_radius, (255, 255, 255), thickness=10, lineType=8, shift=0)
-        binary_image = get_labeled_data(depth_image, scaled_x, scaled_y, scaled_radius)
-        # Resize if only rgb
-        #working_image = cv2.resize(working_image, None, fx=resize_scale, fy=resize_scale, interpolation = cv2.INTER_CUBIC)
-        cv2.imshow("working", working_image)
+        binary_image = generate_bitmap_label(rgb_image, depth_image)
+        cv2.imshow("depth", depth_image)
+        cv2.imshow("binary", binary_image)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+
+def generate_bitmap_label(rgb_image, depth_image):
+    """Takes in 2 numpy arrays representing the raw rgb and depth data.
+
+    Assumes that the rgb and depth data are the same ratio.
+
+    Might have a sideeffect of editing depth data.
+
+    returns:
+        A bitmap if head is detected, where the head is highlighted with white.
+        None if a head is not detected."""
+
+    possible_head_data = get_head_from_img(rgb_image)
+    if possible_head_data is None:
+        return None
+    (x, y, radius) = possible_head_data # Get the head data as ranges from 0-1
+    (x, y, radius) = get_scaled(x, y, radius, depth_image) # Scale data to depth image
+    head_location_dimensions = _get_head_location_dimensions(depth_image, x, y, radius)
+    center_value = _get_center_of_head(head_location_dimensions, depth_image)
+    head_distance = 0.25 # Create threshold of half a meter
+    lower_threshold = center_value - head_distance
+    upper_threshold = center_value + head_distance
+    # Set everything outside threshold to be black, and within to be white
+    white = 255
+    black = 0
+    depth_to_meters = 4.5
+    sub_array_with_head = depth_image[head_location_dimensions]*depth_to_meters
+    threshold_indecies = np.logical_and(
+            lower_threshold < sub_array_with_head, sub_array_with_head < upper_threshold)
+    sub_array_with_head[threshold_indecies] = white
+    sub_array_with_head[sub_array_with_head != white] = black
+    sub_array_with_head[sub_array_with_head == white] = 1
+    bitmap_image = np.zeros(depth_image.shape) # initialize compeletly black label
+    bitmap_image[head_location_dimensions] = sub_array_with_head
+    return bitmap_image
 
 
 def get_scaled(x, y, radius, image):
@@ -52,49 +83,20 @@ def get_scaled(x, y, radius, image):
     return (scaled_x, scaled_y, scaled_radius)
 
 
-def get_labeled_data(depth_image, x, y, radius):
-    """Takes in an image and generates labeled images from it.
-
-    Determines the threshold from the center of the crop where the head is."""
-    # Todo: make this not a cropped image
-    # Todo: account for the rgb to depth mapping
-    depth_to_meters = 4.5
-
-    cropped_image = _get_cropped_image(depth_image, x, y, radius)
-    modified_image = cropped_image*depth_to_meters
-
-    center_x = int(modified_image.shape[1]/2)
-    center_y = int(modified_image.shape[0]/2)
-    center_value = modified_image[center_x, center_y]
-
-    head_distance = 0.25
-
-    # Create threshold of half a meter
-    lower_threshold = center_value - head_distance
-    upper_threshold = center_value + head_distance
-
-    # Set everything outside threshold to be black, and within to be white
-    white = 255
-    black = 0
-
-    threshold_indecies = np.logical_and(
-            lower_threshold < modified_image, modified_image < upper_threshold)
-
-    modified_image[threshold_indecies] = white
-    modified_image[modified_image != white] = black
-    #cv2.imshow("modified", modified_image)
-
-
-def _get_cropped_image(image, x, y, radius):
+def _get_head_location_dimensions(image, x, y, radius):
     width = image.shape[1]
     height = image.shape[0]
     index_crop_left = (x-radius) if (x-radius) > 0 else 0
     index_crop_right = (x+radius) if (x+radius) < width  else width
     index_crop_up = (y-radius) if (y-radius) > 0 else 0
     index_crop_down = (y+radius) if (y+radius) < height else height
-    cropped_image = image[index_crop_up:index_crop_down, index_crop_left:index_crop_right]
+    return  np.index_exp[index_crop_up:index_crop_down, index_crop_left:index_crop_right]
 
-    return cropped_image
+
+def _get_center_of_head(dimensions, image):
+    center_x = dimensions[0].stop-dimensions[0].start
+    center_y = dimensions[1].stop-dimensions[1].start
+    return image[center_x, center_y]
 
 
 if __name__ == "__main__":

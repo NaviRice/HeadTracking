6c167982d0c04b982c55be008c8a467a21f2fe51
roboman2444 <roboman2444@gmail.com>
2018-01-04 13:30:37 -0500

    IR haaar

--- navirice_head_detect.py
+++ navirice_head_detect.py
@@ -1,4 +1,4 @@
-# from navirice_get_image import *
+from navirice_get_image import *
 from navirice_helpers import navirice_image_to_np
 
 import cv2
@@ -24,7 +24,10 @@ def get_head_from_img(np_image):
         Tuple of x, y, and radius values, where domains are 0-1
         None (if head is not detected
     """
-    image = _preprocess_image(np_image)
+
+#i dont bother scaling and graying the image because... its 500 pixels wide and already grayscale
+#    image = _preprocess_image(np_image)
+    image = np_image
     potential_boxes = _run_cascades(image)
 
     if(len(potential_boxes) == 0):
@@ -35,7 +38,7 @@ def get_head_from_img(np_image):
     (x, y, radius) = _get_head_from_boxes(image, potential_boxes)
     print("scaled: {}, {}, {}".format(x, y, radius))
     # Debug print RGB
-    # cv2.imshow("rgb", image)
+    #cv2.imshow("rgb", image)
 
     return (x, y, radius)
 
@@ -57,7 +60,7 @@ def _run_cascades(image):
     boxes = []
     for cascade in cascades:
         boxes = cascade.detectMultiScale(
-            image, scaleFactor=1.1, minNeighbors=5, minSize=(20, 34),
+            image, scaleFactor = 1.1, minNeighbors=5, minSize=(20, 34),
             flags = cv2.CASCADE_SCALE_IMAGE)
         if(len(boxes) != 0):
             break
@@ -87,8 +90,8 @@ def _get_head_from_boxes(image, boxes):
     scaled_radius = max(box_width/image_width, box_height/image_height)/2
 
     # Debug draw circle/rectangle on face
-    # cv2.circle(image, (int(x), int(y)), int(radius), (0, 255, 0), thickness=10, lineType=8, shift=0)
-    #cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.circle(image, (int(x), int(y)), int(radius), (255, 255, 255), thickness=10, lineType=8, shift=0)
+#    cv2.rectangle(image, (x, y), (x+box_width, int(y+box_height)), (255, 255, 255), 2)
 
     return (scaled_x, scaled_y, scaled_radius)
 
@@ -100,8 +103,18 @@ def main():
         img_set, last_count = navirice_get_image(DEFAULT_HOST, DEFAULT_PORT, last_count)
         if(img_set != None):
             print("IR width: {}\nIR height: {}\nIR channels: {}\n".format(img_set.IR.width, img_set.IR.height, img_set.IR.channels))
-            np_image = navirice_image_to_np(img_set.RGB)
+            np_image = navirice_image_to_np(img_set.IR)
+            high = np_image.max()
+#            low = np_image.min()
+ #           np_image -= low
+            np_image /= (high/255) #scale from 0-255, brightest pixel in the image being 255
+            np_image = np.array(np_image, dtype='uint8') # convert to uint8, otherwise cv will freak out (no support for anything other than uint8? wtf? what kind of shit is this?)
+#            np_image.convertTo(np_image, CV_U8)
+#            hist = cv2.calcHist(np_image, channels = 1, mask = cv2.Mat())
+
             get_head_from_img(np_image)
+#            cv2.imshow("IR", np_image) # show preview
+            cv2.imshow("IR", cv2.resize(np_image,None,fx=2.0, fy=2.0, interpolation = cv2.INTER_CUBIC)) #show preview, but bigger
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break

5755368e3f9321bf152ef470becb03e3ec4e7392
Binam Kayastha <binamkayastha@example.com>
2018-02-07 10:40:41 -0500

    Update recording method in navirice_record.
    Now you can have 3 buttons. The first one is just like it was
    previously.
    The next two run haar-cascades on the images, and depending on the mode
    will only save if there is or is not a head detected. This will help us
    avoid saving images of face that the haarcascades don't catch, as well
    as avoid saving images of nonfaces that the haarcascades catch by
    accident.

--- navirice_record.py
+++ navirice_record.py
@@ -1,6 +1,7 @@
 from navirice_get_image import KinectClient
-from navirice_helpers import navirice_img_set_write_file
+from navirice_head_detect import get_head_from_img
 from navirice_helpers import navirice_image_to_np
+from navirice_helpers import navirice_img_set_write_file
 from navirice_helpers import navirice_ir_to_np
 import navirice_image_pb2
 
@@ -9,6 +10,7 @@ from tkinter import *
 import cv2
 import numpy as np
 from threading import Thread
+from enum import Enum
 
 DEFAULT_HOST = '127.0.0.1'  # The remote host
 DEFAULT_PORT = 29000        # The same port as used by the server
@@ -19,32 +21,41 @@ DEFAULT_PORT = 29000        # The same port as used by the server
 #  Modify the protobuf image so that it also includes the information
 #    for the label data. And record label data when recording information.
 
+class State(Enum):
+    STOP = 1
+    RECORDALL = 2
+    RECORDHEAD = 3
+    RECORDNOHEAD = 4
 
 class Window(Frame):
+    """This program is kindof weird. The way it works is that a
+    current_state var is modified, and depending on which one is
+    chosen, a thread will modify the way it saves images."""
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.kinect_client = KinectClient(DEFAULT_HOST, DEFAULT_PORT)
+        self.current_state = State.STOP
         self.master = master
         self.init_window()
 
-
     def init_window(self):
         self.should_pull = True
-        self.should_record = False
         self.should_run = True
         self.session_name = "default"
         self.last_count = 0
         self.master.title("NAVIRICE_RECORDER")
 
-        recordButton = Button(self, text="RECORD/Pause", command=self.record)
-        recordButton.place(x=5, y=0)
+        recordBtn = Button(self, text="RECORD/Pause", command=self.recordAll)
+        recordBtn.place(x=5, y=0)
 
-        recordButtonOnlyHead = Button(self, text="RECORDOnlyHead", command=self.recordHead)
-        recordButtonOnlyHead.place(x=5, y=30)
-        
-        recordButtonNoHead = Button(self, text="RECORDNoHead", command=self.recordNoHead)
-        recordButtonNoHead.place(x=5, y=60)
+        recordBtnOnlyHead = Button(
+                self, text="RECORDOnlyHead", command=self.recordHead)
+        recordBtnOnlyHead.place(x=5, y=30)
+
+        recordBtnNoHead = Button(
+                self, text="RECORDNoHead", command=self.recordNoHead)
+        recordBtnNoHead.place(x=5, y=60)
 
         self.session_text = Text(self, height=1, width=20)
         self.session_text.place(x=5, y=90)
@@ -60,65 +71,98 @@ class Window(Frame):
     def print_states(self):
         self.canvas.delete()
         fill = '#f00'
-        if(self.should_record):
+        if(self.current_state is not State.STOP):
             fill = '#0f0'
         self.canvas.create_oval(4, 0, 25, 25, outline="#000", fill=fill)
         self.canvas.pack(fill=BOTH, expand=1)
-        self.canvas.place(x = 100, y = 0)
+        self.canvas.place(x = 160, y = 0)
 
-    def record(self):
-        self.should_record = not self.should_record
-        self.print_states()
-        name = self.session_text.get("1.0",END)
-        if(len(name)):
-            self.session_name = name
+    def recordAll(self):
+        """Records all images regardless of where head is."""
+        # Todo: need to properly flip boolean here
+        if self.current_state == State.RECORDALL:
+            self.current_state = State.STOP
+        else:
+            self.current_state = State.RECORDALL
+        self._record()
 
     def recordHead(self):
         """Records if head is detected."""
-        if get_head_from_img(np_image) is None:
-            return
-        self.should_record = True
-        self.print_states()
-        name = self.session_text.get("1.0",END)
-        if(len(name)):
-            self.session_name = name
-
+        if self.current_state == State.RECORDHEAD:
+            self.current_state = State.STOP
+        else:
+            self.current_state = State.RECORDHEAD
+        self._record()
 
     def recordNoHead(self):
         """Records if no head is detected."""
-        if get_head_from_img(np_image) is not None:
-            return
-        self.should_record = True
+        if self.current_state == State.RECORDNOHEAD:
+            self.current_state = State.STOP
+        else:
+            self.current_state = State.RECORDNOHEAD
+        self._record()
+
+    def _record(self):
         self.print_states()
         name = self.session_text.get("1.0",END)
         if(len(name)):
             self.session_name = name
+
 
     def kill(self):
         self.should_run = False
 
     def thread_stream(self):
+        img_write_count = 0
         while(self.should_run):
             img_set = None
             if(self.should_pull):
-                img_set, last_count = self.kinect_client.navirice_get_image()
-            if(img_set != None):
-                if self.should_record:
-                    #processThread = Thread(target=navirice_img_set_write_file,
-                    #args=[self.session_name, img_set, self.last_count])
-                    #processThread.start()
-                    navirice_img_set_write_file(self.session_name, img_set, self.last_count)
-                #cv2.imshow("RGB", navirice_image_to_np(img_set.RGB))
-                cv2.imshow("DEPTH", navirice_image_to_np(img_set.Depth))
-                cv2.imshow("IR", navirice_ir_to_np(img_set.IR))
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    print("q pressed in cv window")
-                del img_set
+                img_set, self.last_count = self.kinect_client.navirice_get_image()
+            if(img_set is None):
+                continue
+            print("Current State: {}".format(self.current_state))
+            if self.current_state is not State.STOP:
+                img_write_count +=1
+                _write_file(
+                        self.session_name, img_set, img_write_count,
+                        self.current_state)
+            #cv2.imshow("RGB", navirice_image_to_np(img_set.RGB))
+            cv2.imshow("DEPTH", navirice_image_to_np(img_set.Depth))
+            cv2.imshow("IR", navirice_ir_to_np(img_set.IR))
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                print("q pressed in cv window")
+                return
+            del img_set
+
+
+def _write_file(session_name, img_set, count, current_state):
+    """Saves image, and filters it based on current_state and whether or not
+    a head has been detected."""
+    if current_state is State.STOP:
+        return
+    if current_state is State.RECORDALL:
+        navirice_img_set_write_file(session_name, img_set, count)
+        return
+
+    np_image = navirice_ir_to_np(img_set.IR)
+    head_location = get_head_from_img(np_image)
+
+    if current_state is State.RECORDHEAD and head_location is not None:
+        navirice_img_set_write_file(session_name, img_set, count)
+    elif current_state is State.RECORDNOHEAD and head_location is None:
+        navirice_img_set_write_file(session_name, img_set, count)
 
 
 def main():
+    # count = 0
+    # kinect_client = KinectClient(DEFAULT_HOST, DEFAULT_PORT)
+    # while(count < 1000):
+    #     img_set, last_count = kinect_client.navirice_get_image()
+    #     if img_set is not None:
+    #         last_count = navirice_img_set_write_file("irdance", img_set, count)
+    #         count += 1
     root = Tk()
-    root.geometry("170x65")
+    root.geometry("190x140")
     root.attributes('-type', 'dialog')
     app = Window(root)
     def on_quit():

81e3aa35a0ddebd57735b6b40b34b01267b15f31
roboman2444 <roboman2444@gmail.com>
2018-03-26 15:09:12 -0400

    fixed address already in use bug

--- position_server.py
+++ position_server.py
@@ -9,6 +9,7 @@ class PositionServer:
         self.port = port
         self.host = ''
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.s.bind((self.host, self.port))
         self.s.listen(1)
         self.lock = threading.Lock()

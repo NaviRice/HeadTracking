baf82e5d62e6fc129704513a275fe5106ba89856
Binam Kayastha <binamkayastha@example.com>
2018-04-02 21:48:17 -0400

    Quick hack to get Harry images without kinect

--- src/get_one_img.py
+++ src/get_one_img.py
@@ -1,4 +1,5 @@
 from kinect.kinect_client import KinectClient
+from kinect.fake_kinect_client import FakeKinectClient
 from navirice_helpers import navirice_image_to_np
 from navirice_helpers import navirice_ir_to_np
 
@@ -6,8 +7,19 @@ from navirice_helpers import navirice_ir_to_np
 DEFAULT_HOST = "192.168.1.129"
 DEFAULT_PORT = 29000
 
-kinect_client = KinectClient(DEFAULT_HOST, DEFAULT_PORT)
-kinect_client.navirice_capture_settings(rgb=False, ir=True, depth=True)
+def _get_kinect_client(kinect_type="real"):
+    """Gives back fake kinect client if kinect_type is fake, otherwise 
+    give real one.
+    If you don't have a physical kinect with you, use fake kinect client"""
+    if kinect_type is "fake":
+        kinect_client = FakeKinectClient(DEFAULT_HOST, DEFAULT_PORT)
+    else:
+        kinect_client = KinectClient(DEFAULT_HOST, DEFAULT_PORT)
+    kinect_client.navirice_capture_settings(rgb=False, ir=True, depth=True)
+    return kinect_client
+
+
+kinect_client = _get_kinect_client(kinect_type="fake")
 
 def get_depth_and_ir_from_kinect():
     img_set, last_count = kinect_client.navirice_get_next_image()
